e2ae14b9ccf11ccb42cd0633a417aadb63df55a5
Timbo <tbertenshaw@hotmail.com>
2024-04-30 14:10:23 +0100

    map

--- hello/hello.go
+++ hello/hello.go
@@ -10,11 +10,13 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message,err := greetings.Hello("Tim")
+	names := []string{"Tim","Gladys","Fred"}
+
+	messages,err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
